7cefdeb263fe9afdc42c937f52740cd6a8d1d97a
zxcbog <123>
2025-09-22 13:08:38 +0000

    rag filestructure fix

--- rag_service/src/main.py
+++ rag_service/src/main.py
@@ -14,4 +14,4 @@ def create_app() -> FastAPI:
 
 if __name__ == "__main__":
     app = create_app()
-    uvicorn.run(app, host="0.0.0.0", port=os.environ['PORT'])
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ['PORT']))
